1745de63d137d8f49c76d211dc0076aad5fb2e1b
Shivan Singh <shivan.0972@gmail.com>
2022-01-16 16:04:37 +0530

    use small v for build

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ done
 version=$(cat module.prop | grep version= | cut -d= -f2)
 rm -rf *.zip
 sed -i -e "s/DEVICE_USES_VOLUME_KEY=0/DEVICE_USES_VOLUME_KEY=1/g" module.prop
-7z a Pixelify-V$version.zip *
+7z a Pixelify-v$version.zip *
 sed -i -e "s/DEVICE_USES_VOLUME_KEY=1/DEVICE_USES_VOLUME_KEY=0/g" module.prop
-7z a Pixelify-V$version-no_VK.zip $(ls | grep -v *.zip)
+7z a Pixelify-v$version-no_VK.zip $(ls | grep -v *.zip)
 mkdir -p out
 rm -rf out/*
 mv Pixelify-V$version.zip out
